8a3f4359cbd0fb65da9e219d6a820cc75bc94b5b
erickandreww <erick.andrew.moura@gmail.com>
2025-02-01 14:46:16 -0300

    "login page initial"

---
+++ app/login/page.tsx
@@ -0,0 +1,14 @@
+// import { Suspense } from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Login'
+}
+
+export default function LoginPage() {
+  return (
+    <main className="">
+      <p>Login Page</p>
+    </main>
+  );
+}
