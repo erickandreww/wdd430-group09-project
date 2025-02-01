27f0b5a1f012fa61cbc110f82e3f50b91ee1deec
erickandreww <erick.andrew.moura@gmail.com>
2025-02-01 14:47:51 -0300

    "Home page Hero"

---
+++ app/ui/home-page/hero.tsx
@@ -0,0 +1,16 @@
+import Image from 'next/image';
+// import Link from 'next/link';
+import Button from '../button';
+
+
+export default function Hero() {
+  return (
+    <div className="">
+        <h2>Description of the hero</h2>
+        <Button className='flex h-7 items-center bg-slate-400 px-3 text-sm font-medium text-black transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50'>
+            Click Me
+        </Button>
+        <Image src='/placeholder.jpg' alt='placeholder for now' width={300} height={300}/>
+    </div>
+  );
+}
