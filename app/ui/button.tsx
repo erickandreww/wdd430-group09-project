cf131d6e5385d514163f8db2bddded22d94f62ba
erickandreww <erick.andrew.moura@gmail.com>
2025-02-01 14:47:15 -0300

    "setting button"

---
+++ app/ui/button.tsx
@@ -0,0 +1,16 @@
+import clsx from "clsx";
+
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+}
+
+export default function Button({children, className, ...rest}: ButtonProps) {
+  return (
+    <button 
+    {...rest}
+    className={clsx('', className)}
+    >
+      {children}
+    </button>
+  );
+}
