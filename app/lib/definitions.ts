b56996fefd7e23f1f7a7dfdfcc3320b974db1ae4
erickandreww <erick.andrew.moura@gmail.com>
2025-02-08 14:55:19 -0300

    "setting ProductsCard definition"

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -1,8 +1,11 @@
 
 export type ProductsCard = {
-  id: number;
-  name: string;
-  description: string;
-  image: string;
-  price: string;
+  product_id: number;
+  product_name: string;
+  product_image: string;
+  product_price: number;
+  product_description: string;
+  product_quantity: number; 
+  user_id: string;
+  purchase_number: string;
 }
