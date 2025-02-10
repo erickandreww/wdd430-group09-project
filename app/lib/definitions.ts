24f648f2b90ef7a7c1b7e4712862f6281c24704b
erickandreww <erick.andrew.moura@gmail.com>
2025-02-10 00:34:38 -0300

    "products page update"

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -10,6 +10,23 @@ export type ProductsCard = {
   purchase_number: string;
 }
 
+export type FeaturedProducts = {
+  product_id: number;
+  product_name: string;
+  product_image: string;
+  product_price: number;
+  product_description: string;
+}
+
+export type ProductsInfo = {
+  product_id: number;
+  product_name: string;
+  product_image: string;
+  product_price: number;
+  product_description: string;
+  product_quantity: number; 
+}
+
 export type UsersDefinitions = {
   id: number;
   name: string;
--- app/ui/products/product.tsx
+++ app/ui/products/product.tsx
@@ -1,23 +1,54 @@
+
 import Image from "next/image";
-import { getProductById } from "@/app/lib/data";
+import { auth } from "@/auth";
+import { getProductById, GetUserIdByEmail } from "@/app/lib/data";
+
+function getQuantity(quantity:number) {
+  const options: object[] = [];
+
+  for (let i = 0; i <= quantity; i++) {
+    const opt = <option value={i}>{i}</option>
+    options.push(opt)
+  }
+
+  return options;
+}
 
 export default async function GetProduct({id}: {id:string}) {
   const product = await getProductById(id);
 
-    return (
-      <div className="flex p-5">
-        <Image 
-        src={`/products/${product.product_image}`}
-        alt={product.product_name} 
-        width={400}
-        height={400}></Image>
-        <div>
-          <h2 className="text-lg font-semibold">{product.product_name}</h2>
-          <p className="text-sm text-gray-600">{product.product_description}</p>
-          <p className="text-xl font-bold mt-2">{product.product_price}</p>
-          <p className="text-xl font-bold mt-2">{product.product_quantity}</p>
-          <p className="text-xl font-bold mt-2">{product.name}</p>
-        </div>
+  const session = await auth()
+  const email = session?.user?.email
+  const user_id = await GetUserIdByEmail(email);
+  console.log(user_id)
+
+  const quantity = getQuantity(product.product_quantity)
+  console.log(quantity)
+
+  return (
+    <div className="" key={product.product_id}>
+      <Image 
+      src={`/products/${product.product_image}`}
+      alt={product.product_name} 
+      width={400}
+      height={400}></Image>
+      <div>
+        <h2 className="text-lg font-semibold">{product.product_name}</h2>
+        <p className="text-sm text-gray-600">{product.product_description}</p>
+        <p className="text-xl font-bold mt-2">{product.product_price}</p>
+        <p className="text-xl font-bold mt-2">{product.name}</p>
       </div>
-    )
+      {/* <div className="grid grid-rows-2">
+        <label htmlFor="quantity-selected" className="">Choose a Quantity</label>
+        <select name="quantity" id="quantity" className="w-12">
+          {quantity.map(option => {
+            return option
+          })}
+        </select>
+      </div> */}
+      {/* <button onClick={(e) => {
+        addProductToCart(user_id, "1", id)
+      }}>Add to Cart</button> */}
+    </div>
+  )
 }
