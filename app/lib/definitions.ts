799813d12026bd919d5cbb57490ccc5b43313891
erickandreww <erick.andrew.moura@gmail.com>
2025-02-13 23:00:08 -0300

    "adding ReviewsList type"

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -36,11 +36,17 @@ export type UsersDefinitions = {
 }
 
 export type ProductToCart = {
-  product_name: string;
-  product_image: string;
-  product_price: number;
-  product_description: string;
   quantity: number;
-  user_id: string; 
+  user_id: number; 
   product_id: string;
+}
+
+export type ReviewsList = {
+  review_id: string;
+  rating: number;
+  review_text: string;
+  review_date: string;
+  product_id: string;
+  product_name: string; 
+  name: string; 
 }
