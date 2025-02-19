f2c8f7138495115b99bd324936d2a42d38972e39
erickandreww <erick.andrew.moura@gmail.com>
2025-02-19 20:37:09 -0300

    "Home page products improvement"

--- app/ui/home-page/featured-products.tsx
+++ app/ui/home-page/featured-products.tsx
@@ -3,31 +3,31 @@ import Link from 'next/link';
 import { fetchFeaturedProducts } from '@/app/lib/data';
 
 export default async function FeatureHomeProducts() {
-    const featuredProducts = await fetchFeaturedProducts();
+  const featuredProducts = await fetchFeaturedProducts();
   return (
-    <div>
-      <h2>Featured Products</h2>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-4 px-5">
+    <div className="py-10">
+      <h2 className="text-3xl font-bold text-color_three text-center mb-8">Featured Products</h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {featuredProducts?.map(product => (
           <Link key={product.product_id} href={`products/${product.product_id}`}>
-          <div className="border rounded-lg overflow-hidden shadow-lg border-color_three bg-color_four flex flex-col min-h-[370px]">
-            <div className="w-full h-64 relative">
-              <Image 
-                src={`${product.product_image}`}
-                alt={product.product_name} 
-                width={0} 
-                height={0} 
-                className="absolute inset-0 w-full h-full object-cover"
-                layout="fill"
-              />
+            <div className="border rounded-lg overflow-hidden shadow-lg border-color_three bg-color_four flex flex-col h-full transition-transform transform hover:scale-105 hover:shadow-xl">
+              <div className="w-full h-64 relative">
+                <Image 
+                  src={product.product_image}
+                  alt={product.product_name} 
+                  layout="fill"
+                  objectFit="cover"
+                  className="absolute inset-0 w-full h-full"
+                />
+              </div>
+              <div className="p-4 flex-1 text-center flex flex-col justify-between">
+                <div>
+                  <h3 className="text-2xl font-bold text-color_three mb-3">{product.product_name}</h3>
+                  <p className="text-xl font-bold text-color_three">R$ {product.product_price}</p>
+                </div>
+              </div>
             </div>
-            <div className="p-2 flex-1 text-center">
-              <h3 className="text-xl font-semibold text-color_three">{product.product_name}</h3>
-              <p className="text-sm p-1 text-foreground">{product.product_description}</p>
-              <p className="text-xl font-bold mt-2 text-color_three">R$ {product.product_price}</p>
-            </div>
-          </div>
-        </Link>
+          </Link>
         ))}
       </div>
     </div>
